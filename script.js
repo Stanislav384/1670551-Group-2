493d9c253fb0fa1a8cbecf0d4d989c9d060547c8
isnikx0 <isaievnikitaa@gmail.com>
2025-08-31 20:18:59 +0200

    добавлен typewriter, добавлены базовые стили для главной страницы. итог: главная страница готова

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const text= "NFS- your helper in organization Stay organized, stay yourself";
+const text = "NFS- your helper in organization    Stay organized, stay yourself";
 let i = 0;
 
-function typeWriter()  {
+function typeWriter() {
     if (i < text.length) {
-        odcument.getElementbyId("typewriter").innerHTML += text.charAt(i);
+        document.getElementById("typewriter").innerHTML += text.charAt(i);
         i++;
         setTimeout(typeWriter, 100);
     }
 }
 
-typeWriter()
+typeWriter();
